f78c4fc9addbebcb12fd1874c3e1e544770125d7
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-01-29 08:54:41 +0100

    Update telebot.py 20

--- telebot.py
+++ telebot.py
@@ -252,7 +252,7 @@ async def detect_intent(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await redirect_to_private(update, context, "feedback")
         elif any(keyword in message.text.lower() for keyword in intents["help"]):
             await redirect_to_private(update, context, "help")
-		elif any(keyword in message.text.lower() for keyword in intents["onboarding"]):
+	elif any(keyword in message.text.lower() for keyword in intents["onboarding"]):
             await redirect_to_private(update, context, "onboarding")
         elif any(keyword in message.text.lower() for keyword in intents["support"]):
             await redirect_to_private(update, context, "support")
